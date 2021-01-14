f60c142349fe5f3407d654d68ae47cf445c40d1e
theriley106 <christopherlambert106@gmail.com>
2021-01-14 02:03:44 -0800

    Updated all

--- app.py
+++ app.py
@@ -1,48 +1,32 @@
 from flask import Flask, render_template, jsonify
 import os
 import json
+import getData
 
 app = Flask(__name__, static_url_path='/static')
-
-RESPONSE = {}
-FILE_NAME = 'response.json'
 
 @app.template_filter()
 def currencyFormat(value):
     value = float(value)
     return "${:,.0f}".format(value * 1000000)
-
-def get_updated_time(fileName):
-    try:
-        return int(os.path.getmtime(fileName))
-    except:
-        return None
-
-def get_net_worth():
-    last_updated = RESPONSE.get('updated_at', 0)
-    if get_updated_time(FILE_NAME) != RESPONSE.get('modified', 0):
-        if os.path.exists(FILE_NAME):
-            RESPONSE['response'] = json.load(open(FILE_NAME))
-            RESPONSE['modified'] = get_updated_time(FILE_NAME)
-    return RESPONSE.get('response', {})
     
 @app.route('/', methods=['GET'])
 def index():
-    ranking = get_net_worth().get('ranking', [])
+    ranking = getData.get_from_mongo().get('ranking', [])
     if len(ranking) < 5:
         return 'error', 404
     return render_template("index.html", ranking=ranking, result=ranking[0]["personName"] == "Elon Musk")
 
 @app.route('/no', methods=['GET'])
 def indexno():
-    ranking = get_net_worth().get('ranking', [])
+    ranking = getData.get_from_mongo().get('ranking', [])
     if len(ranking) < 5:
         return 'error', 404
     return render_template("index.html", ranking=ranking, result=ranking[0]["personName"] != "Elon Musk")
 
 @app.route('/debug', methods=['GET'])
 def debug():
-    return jsonify(get_net_worth())
+    return jsonify(getData.get_from_mongo())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
--- getData.py
+++ getData.py
@@ -4,8 +4,9 @@ try:
     from keys import *
 except:
     API_KEY = os.environ['API_KEY']
+    MONGO_URL = os.environ['MONGO_URL']
 import time
-import json
+import pymongo
 
 RANKING_URL = "https://www.forbes.com/forbesapi/person/rtb/0/-estWorthPrev/true.json?fields=rank,personName,finalWorth,estWorthPrev,uri"
 QUOTE_URL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={0}&apikey={1}"
@@ -15,6 +16,11 @@ ELON_MUSK_SHARES = 34098597
 
 ELON_NW_NO_STOCK = (183763.418 * 1000000) - (ELON_MUSK_SHARES * 849.44)
 BEZOS_NW_NO_STOCK = (182416.835 * 1000000) - (JEFF_BEZOS_SHARES * 3120.83)
+
+client = pymongo.MongoClient(MONGO_URL)
+db = client.test
+db = client['Stonks']
+collection = db['StonkResponse']
 
 def get_share_price(ticker):
     for i in range(3):
@@ -31,6 +37,15 @@ def get_ranking():
             time.sleep(i + 1)
     return []
 
+def save_to_mongo(response):
+    collection.insert_one(response)
+
+def get_from_mongo():
+    for val in collection.find({'type': 'response'}).sort("modified_time", pymongo.DESCENDING):
+        x = val
+        del x['_id']
+        return x
+    return {}
 
 if __name__ == "__main__":
     bezos_nw = (BEZOS_NW_NO_STOCK + (get_share_price('AMZN') * JEFF_BEZOS_SHARES)) / 1000000
@@ -47,9 +62,4 @@ if __name__ == "__main__":
         val['rank'] = i + 1
     
     if len(ranking) != 0:
-        response = {
-            "ranking": ranking[:5],
-            "updated_at": time.time()
-        }
-        with open('response.json', 'w') as f:
-            json.dump(response, f, indent=4)
+        save_to_mongo({"ranking": ranking[:5], 'type': 'response', 'modified_time': int(time.time())})
